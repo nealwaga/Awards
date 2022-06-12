24026d40aef165a0323d2cdf95b1ace23abb7ac4
nealwaga <waganeal@gmail.com>
2022-06-12 19:03:32 +0300

    Updated models in admin page

--- awards/admin.py
+++ awards/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
 admin.site.register (UserProfile)
 admin.site.register (Rate)
 admin.site.register (Site)
 admin.site.register (tags)
+
+
+#Create here
+class SiteAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags', )
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=['user', 'bio', 'phone_number', 'is_mvp']
+    list_editable=[ 'bio', 'phone_number', 'is_mvp']
