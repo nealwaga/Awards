ead69617c083c859c13177a5b761e0f0d078d020
nealwaga <waganeal@gmail.com>
2022-06-13 14:29:19 +0300

    Deployment

--- project/settings.py
+++ project/settings.py
@@ -54,6 +54,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
